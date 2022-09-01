d1dc5c3d18569cabc484761048c642793bd30f97
Jakub <jaqbec@gmail.com>
2022-09-01 13:59:49 +0200

    fix server

--- server/index.js
+++ server/index.js
@@ -1,116 +1,98 @@
-const http = require("http"),
-    express = require("express"),
-    app = express(),
-    socketIo = require("socket.io");
-const fs = require("fs");
+const express = require("express");
+const app = express();
+const PORT = 4000;
 
-const server = http.Server(app).listen(8080);
-const io = socketIo(server);
+const http = require("http").Server(app);
+const cors = require("cors");
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
 const clients = {};
+const players = {};
+let unmatched = null;
 
-app.use(express.static(__dirname + "/../client/"));
-app.use(express.static(__dirname + "/../node_modules/"));
+const addClient = (socket) => {
+  clients[socket.id] = socket;
+};
+const removeClient = (socket) => {
+  delete clients[socket.id];
+};
+const getOpponent = (socket) => {
+  if (!players[socket.id].opponent) {
+    return;
+  }
 
-app.get("/", (req, res) => {
-    // res.sendFile("index.html", { root: __dirname + "/../client" });
-    const stream = fs.createReadStream(__dirname + "/../client/index.html");
-    stream.pipe(res);
+  return players[players[socket.id].opponent].socket;
+};
+const joinGame = (socket) => {
+  players[socket.id] = {
+    opponent: unmatched,
+    symbol: "X",
+    socket: socket,
+  };
+
+  if (unmatched) {
+    players[socket.id].symbol = "O";
+    players[unmatched].opponent = socket.id;
+    unmatched = null;
+  } else {
+    unmatched = socket.id;
+  }
+};
+
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
 });
 
-const addClient = socket => {
-    console.log("New client connected", socket.id);
-    clients[socket.id] = socket;
-};
-const removeClient = socket => {
-    console.log("Client disconnected", socket.id);
-    delete clients[socket.id];
-};
-
-io.sockets.on("connection", socket => {
-    let id = socket.id;
-
-    addClient(socket);
-
-    socket.on("mousemove", data => {
-        data.id = id;
-        socket.broadcast.emit("moving", data);
-    });
-
-    socket.on("disconnect", () => {
-        removeClient(socket);
-        socket.broadcast.emit("clientdisconnect", id);
-    });
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
 });
 
-var players = {},
-    unmatched;
+socketIO.on("connection", function (socket) {
+  console.log(`🚀 socketIO.on: ${socket.id} user just connected!`);
+  let id = socket.id;
 
-function joinGame(socket) {
-    // Add the player to our object of players
-    players[socket.id] = {
-        // The opponent will either be the socket that is
-        // currently unmatched, or it will be null if no
-        // players are unmatched
-        opponent: unmatched,
+  addClient(socket);
 
-        // The symbol will become 'O' if the player is unmatched
-        symbol: "X",
+  joinGame(socket);
 
-        // The socket that is associated with this player
-        socket: socket
-    };
+  if (getOpponent(socket)) {
+    socket.emit("game_begin", {
+      symbol: players[socket.id].symbol,
+    });
 
-    // Every other player is marked as 'unmatched', which means
-    // there is not another player to pair them with yet. As soon
-    // as the next socket joins, the unmatched player is paired with
-    // the new socket and the unmatched variable is set back to null
-    if (unmatched) {
-        players[socket.id].symbol = "O";
-        players[unmatched].opponent = socket.id;
-        unmatched = null;
-    } else {
-        unmatched = socket.id;
-    }
-}
+    getOpponent(socket).emit("game_begin", {
+      symbol: players[getOpponent(socket).id].symbol,
+    });
+  }
 
-// Returns the opponent socket
-function getOpponent(socket) {
-    if (!players[socket.id].opponent) {
-        return;
+  socket.on("make_move", function (data) {
+    if (!getOpponent(socket)) {
+      return;
     }
 
-    return players[players[socket.id].opponent].socket;
-}
+    socket.emit("move_made", data);
+    getOpponent(socket).emit("move_made", data);
 
-io.on("connection", function(socket) {
-    joinGame(socket);
+    return true;
+  });
 
-    // Once the socket has an opponent, we can begin the game
+  socket.on("disconnect", function () {
     if (getOpponent(socket)) {
-        socket.emit("game.begin", {
-            symbol: players[socket.id].symbol
-        });
-
-        getOpponent(socket).emit("game.begin", {
-            symbol: players[getOpponent(socket).id].symbol
-        });
+      getOpponent(socket).emit("opponent_left");
+      removeClient(socket);
     }
+  });
 
-    // Listens for a move to be made and emits an event to both
-    // players after the move is completed
-    socket.on("make.move", function(data) {
-        if (!getOpponent(socket)) {
-            return;
-        }
-
-        socket.emit("move.made", data);
-        getOpponent(socket).emit("move.made", data);
-    });
-
-    // Emit an event to the opponent when the player leaves
-    socket.on("disconnect", function() {
-        if (getOpponent(socket)) {
-            getOpponent(socket).emit("opponent.left");
-        }
-    });
+  socket.on("mouse_move", (data) => {
+    data.id = id;
+    socket.broadcast.emit("moving", data);
+  });
 });
