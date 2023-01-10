3ed1aceec849c2a2a0cddfbb9ae783474f3dc264
Jakub <jaqbec@gmail.com>
2023-01-10 12:32:20 +0100

    header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
     <div className="app">
       <header className="app-header">
+        <p>Magic chat app</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
